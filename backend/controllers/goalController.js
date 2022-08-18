95b54ad2f372b3a61d6a64d7dd378d4c7cd3d421
nansmatty <narayanmaitysp1997@gmail.com>
2022-08-18 18:00:36 +0530

    db connected

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,46 @@
 // @desc  GET GOALS
 // @route GET /api/goals
+
+const asyncHandler = require("express-async-handler");
+
 // @access Private
-exports.getGoals = (req, res) => {
-	res.status(200).json({ messsage: "Get Goals" });
-};
+exports.getGoals = asyncHandler(
+	async (req, res) => {
+		res
+			.status(200)
+			.json({ messsage: "Get Goals" });
+	}
+);
 
 // @desc  SET GOAL
 // @route POST /api/goals
 // @access Private
-exports.setGoal = (req, res) => {
-	res.status(200).json({ messsage: "Set Goals" });
-};
+exports.setGoal = asyncHandler(
+	async (req, res) => {
+		res
+			.status(200)
+			.json({ messsage: "Set Goals" });
+	}
+);
 
 // @desc  UPDATE GOAL
 // @route PUT /api/goals/:id
 // @access Private
-exports.updateGoal = (req, res) => {
-	res.status(200).json({
-		messsage: `Update Goals ${req.params.id}`,
-	});
-};
+exports.updateGoal = asyncHandler(
+	async (req, res) => {
+		res.status(200).json({
+			messsage: `Update Goals ${req.params.id}`,
+		});
+	}
+);
 
 // @desc  DELETE GOAL
 // @route DELETE /api/goals/:id
 // @access Private
-exports.deleteGoal = (req, res) => {
-	res.status(200).json({
-		messsage: `Delete Goals ${req.params.id}`,
-	});
-};
+exports.deleteGoal = asyncHandler(
+	async (req, res) => {
+		res.status(200).json({
+			messsage: `Delete Goals ${req.params.id}`,
+		});
+	}
+);
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const colors = require("colors");
+const {
+	errorHandler,
+} = require("./middleware/errorMiddleware");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const goalRoutes = require("./routes/goalRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
+
+app.use(errorHandler);
 
 app.listen(
 	port,
