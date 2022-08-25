c4856d6106b93adc7a453ae73e6b3e8ed36ac80c
nansmatty <narayanmaitysp1997@gmail.com>
2022-08-25 23:26:35 +0530

    some minor twaeks

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const path = require("path");
 const {
 	errorHandler,
 } = require("./middleware/errorMiddleware");
@@ -18,6 +19,31 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/goals", goalRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+	app.use(
+		express.static(
+			path.join(__dirname, "../frontend/build")
+		)
+	);
+
+	app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				"../",
+				"frontend",
+				"build",
+				"index.html"
+			)
+		)
+	);
+} else {
+	app.get("/", (req, res) =>
+		res.send("Please set to production")
+	);
+}
 
 app.use(errorHandler);
 
